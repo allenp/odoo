4ecb0ad11bce32adbc0a231c8f7201e0b5440c1c
Paul Allen <paulallen.com.jm@gmail.com>
2016-01-03 00:37:42 -0500

    Use pyusb-scale module for scale.

--- addons/hw_scale/controllers/main.py
+++ addons/hw_scale/controllers/main.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
-import time
-from os.path import join
-from threading import Thread, Lock
-from select import select
-from Queue import Queue, Empty
 
 import openerp
 import openerp.addons.hw_proxy.controllers.main as hw_proxy
@@ -13,196 +7,30 @@ from openerp import http
 from openerp.http import request
 from openerp.tools.translate import _
 
+from scale.scale import Scale
+
 _logger = logging.getLogger(__name__)
 
-VENDOR_ID = 0x0922
-PRODUCT_ID = 0x8003
-DATA_MODE_GRAMS = 2
-DATA_MODE_OUNCES = 11
-
-try:
-    import usb.core as usbcore;
-    import usb.util
-except ImportError:
-    _logger.error('Odoo module hw_scale_usb depends on the usb.core and usb.util modules')
-    usbcore = False
-
-
-class UsbScale(Thread):
-
-    def __init__(self):
-        Thread.__init__(self)
-        self.lock = Lock()
-        self.scalelock = Lock()
-        self.status = {'status':'connecting', 'messages':[]}
-        self.weight = 0
-        self.weight_info = 'ok'
-        self.device = None
-
-    def lockedstart(self):
-        with self.lock:
-            if not self.isAlive():
-                self.daemon = True
-                self.start()
-
-    def set_status(self, status, message = None):
-        if status == self.status['status']:
-            if message is not None and message != self.status['messages'][-1]:
-                self.status['messages'].append(message)
-
-                if status == 'error' and message:
-                    _logger.error('Scale Error: '+message)
-                elif status == 'disconnected' and message:
-                    _logger.warning('Disconnected Scale: '+message)
-        else:
-            self.status['status'] = status
-            if message:
-                self.status['messages'] = [message]
-            else:
-                self.status['messages'] = []
-
-            if status == 'error' and message:
-                _logger.error('Scale Error: '+message)
-            elif status == 'disconnected' and message:
-                _logger.warning('Disconnected Scale: %s', message)
-
-    def get_device(self):
-        try:
-            self.device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
-
-            interface = 0
-
-            if self.device is None:
-              return self.device
-
-            if self.device.is_kernel_driver_active(interface) is True:
-              self.device.detach_kernel_driver(interface)
-              self.device.set_configuration()
-              usbcore.util.claim_interface(self.device, interface)
-
-            self.set_status('connected','Connected to '+ 'Device name here')
-            return self.device
-        except Exception as e:
-            self.set_status('error',str(e))
-            return None
-
-    def get_weight(self):
-        self.lockedstart()
-        return self.weight
-
-    def get_weight_info(self):
-        self.lockedstart()
-        return self.weight_info
-
-    def get_status(self):
-        self.lockedstart()
-        return self.status
-
-    def read_weight(self):
-        with self.scalelock:
-            if self.device:
-                try:
-                  endpoint = self.device[0][(0,0)][0]
-                  preload = 3
-                  while preload > 0:
-                    self.device.read(endpoint.bEndpointAddress,
-                                endpoint.wMaxPacketSize)
-                    preload -= 1
-
-                    attempts = 10
-                    data = None
-                    while data is None and attempts > 0:
-                      try:
-                        data = self.device.read(endpoint.bEndpointAddress,
-                                            endpoint.wMaxPacketSize)
-                      except usb.core.USBError as e:
-                        data = None
-                        if e.args == ('Operation timed out',):
-                          attempts -= 10
-                          continue
-
-                    raw_weight = data[4] + data[5] * 256
-
-                    if data[2] == DATA_MODE_OUNCES:
-                      ounces = raw_weight * 0.1
-                      weight = ounces
-                    elif data[2] == DATA_MODE_GRAMS:
-                      grams = raw_weight
-                      weight = grams * .035274
-
-                    return weight
-                except Exception as e:
-                    self.set_status('error',str(e))
-                    self.device = None
-
-    def set_zero(self):
-        with self.scalelock:
-            if self.device:
-                try:
-                  print('Not implemented')
-                except Exception as e:
-                    self.set_status('error',str(e))
-                    self.device = None
-
-    def set_tare(self):
-        with self.scalelock:
-            if self.device:
-                try:
-                  print('Not implemented')
-                except Exception as e:
-                    self.set_status('error',str(e))
-                    self.device = None
-
-    def clear_tare(self):
-        with self.scalelock:
-            if self.device:
-                try:
-                  print('Not implemented')
-                except Exception as e:
-                    self.set_status('error',str(e))
-                    self.device = None
-
-    def run(self):
-        self.device = None
-
-        while True:
-            if self.device:
-                self.weight = self.read_weight()
-                time.sleep(0.3)
-            else:
-                with self.scalelock:
-                    self.device = self.get_device()
-                if not self.device:
-                    time.sleep(5)
-
-scale_thread = None
-
-if usbcore:
-    scale_thread = UsbScale()
-    hw_proxy.drivers['scale'] = scale_thread
+usb_scale = Scale()
+hw_proxy.drivers['scale'] = usb_scale
 
 class ScaleDriver(hw_proxy.Proxy):
     @http.route('/hw_proxy/scale_read/', type='json', auth='none', cors='*')
     def scale_read(self):
-        if scale_thread:
-            return {'weight': scale_thread.get_weight(), 'unit':'kg', 'info': scale_thread.get_weight_info()}
+        if usb_scale:
+          reading = usb_scale.weigh()
+          return { 'weight': reading.weight, 'unit': reading.unit, 'info': reading.status }
         return None
 
     @http.route('/hw_proxy/scale_zero/', type='json', auth='none', cors='*')
     def scale_zero(self):
-        if scale_thread:
-            scale_thread.set_zero()
         return True
 
     @http.route('/hw_proxy/scale_tare/', type='json', auth='none', cors='*')
     def scale_tare(self):
-        if scale_thread:
-            scale_thread.set_tare()
         return True
 
     @http.route('/hw_proxy/scale_clear_tare/', type='json', auth='none', cors='*')
     def scale_clear_tare(self):
-        if scale_thread:
-            scale_thread.clear_tare()
         return True
 
